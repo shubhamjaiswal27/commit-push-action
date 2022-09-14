25067c466f92d336d80a5d8fae27749e1acd0049
Shubham Jaiswal <shubhamjaiswal27@github.com>
2022-09-14 13:19:35 +0530

    change the entrypoint to fix issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-git config user.name $INPUT_GIT_USERNAME
-git config user.email $INPUT_GIT_EMAIL
+git config user.name "${INPUT_GIT_USERNAME}"
+git config user.email "${INPUT_GIT_EMAIL}"
 
 git add .
-git commit -m $INPUT_COMMIT_MESSAGE
+git commit -m "${INPUT_COMMIT_MESSAGE}"
 
 git fetch origin $INPUT_BRANCH
 git push origin HEAD:$INPUT_BRANCH
